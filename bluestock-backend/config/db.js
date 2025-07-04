95b44c765f9585f2933a0633dd47ffd600c286cc
Ocean <Ocean@MacBook-Air.local>
2025-07-05 04:00:33 +0530

    Fix: Cloud PostgreSQL connection using DATABASE_URL

--- bluestock-backend/config/db.js
+++ bluestock-backend/config/db.js
@@ -1,11 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "Ocean",     
-  host: "localhost",
-  database: "bluestock",
-  password: "",         
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 pool.connect()
